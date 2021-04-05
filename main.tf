db59e7c6b9d748fcc8f81ce6f44626622b525e74
Vivek Gopalakrishnan <vivekgopalakrishnan@Anvis-MacBook-Air.local>
2021-04-05 21:08:28 +0530

    creating terraform script

---
+++ main.tf
@@ -0,0 +1,55 @@
+provider "aws" {
+        region = "ap-south-1"
+}
+
+#Creating security group and allowing SSH and HTTP
+
+resource "aws_security_group" "webpage-terra-ssh-http" {
+         name = "webpage-terra-ssh-http"
+         description = "allowing SSH and HTTP traffic" 
+
+         ingress {
+                from _port = 22
+                to_port = 22
+                protocol = "tcp"
+                cidr_blocks = ["0.0.0.0/0"]
+        }
+        
+        ingress {
+                from _port = 80
+                to_port = 80
+                protocol = "tcp"
+                cidr_blocks = ["0.0.0.0/0"]
+       }
+    
+        egress {
+                from _port = 0
+                to_port = 0
+                protocol = "-1"
+                cidr_blocks = ["0.0.0.0/0"]
+        }         
+}
+
+# Closing the security group here
+
+# Creating an AWS Ec2 instance 
+
+resource "aws_instance" "webpage-terra" {
+         ami = "ami-0bcf5425cdc1d8a85"
+         instance_type = "t2.micro"
+         availability_zone = "ap-south-1a"
+         security_groups = ["${aws_security_group.webpage-terra-ssh-http.name}"]
+         key_name = "Viv2
+         user_data = <<-EOF
+                 #! /bin/bash
+                 sudo yum install httpd -y
+                 sudo systemctl start httpd
+                 sudo systemctl enable httpd
+                 echo "<h1>Creation of a web server on terraform<br>version 1.0.0.0.1<h1>" >> /var/www/html.index.html
+         EOF
+         
+         tags = {
+                  Name = "webserver"
+                  }
+
+}
